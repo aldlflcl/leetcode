30aee13635107c5d3453191719cacc8f0ac03486
aldlflcl <64316701+aldlflcl@users.noreply.github.com>
2022-04-10 08:27:27 +0900

    Time: 0 ms (100.00%), Space: 40.9 MB (45.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-      int[][] arr = new int[46][46];
+      int[] arr = new int[46];
       return solution(n, 0, arr);
     }
   
-    public int solution(int n, int cur, int[][] arr) {
+    public int solution(int n, int cur, int[] arr) {
       if ((n - cur) < 0) return 0;
       if ((n - cur) == 0) return 1;
       
-      if (arr[n][cur] != 0) return arr[n][cur];
+      if (arr[cur] != 0) return arr[cur];
       
-      return arr[n][cur] = solution(n, cur + 1, arr) + solution(n, cur + 2, arr);
+      return arr[cur] = solution(n, cur + 1, arr) + solution(n, cur + 2, arr);
     }
 }
